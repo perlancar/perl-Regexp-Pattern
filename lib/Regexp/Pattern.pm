3077305debdef2c527ff0d60cec28e43034fa3ff
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-20 15:36:46 +0700

    Initial commit (not yet)

---
+++ lib/Regexp/Pattern.pm
@@ -0,0 +1,22 @@
+package Regexp::Pattern;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of regexp patterns
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This is an alternative to C<Regexp::Common::*>. The main focus of
+Regexp::Pattern::* is low startup overhead. There's a bridge module
+L<Regexp::Common::RegexpPattern> to use patterns in C<Regexp::Pattern::*>
+modules via Regexp::Common.
+
+
+=head1 SEE ALSO
+
+L<Regexp::Common>
